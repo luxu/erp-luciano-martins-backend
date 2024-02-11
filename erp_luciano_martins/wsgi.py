1815d9818746b81649f8f031737d4884ee454134
Luciano Martins <zicadopv@gmail.com>
2024-02-11 17:43:45 -0300

    Config vercel

--- erp_luciano_martins/wsgi.py
+++ erp_luciano_martins/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for erp_luciano_martins project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "erp_luciano_martins.settings")
 
 application = get_wsgi_application()
+app = get_wsgi_application()
