36e768c3d951587013dd44ad00fa4d55908f0725
Luciano Martins <luciano.martins@quay.com.br>
2026-05-10 23:02:17 -0300

    Retired lib: django-extension

--- erp_luciano_martins/settings.py
+++ erp_luciano_martins/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_extensions",
+    # "django_extensions",
     "ninja_extra",
     "ninja_jwt",
     "core",
